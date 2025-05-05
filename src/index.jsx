9c29ffc79991c17e55cdec824046b42d3760d4e6
Petr <sapryckinpetr4@mail.ru>
2025-05-05 19:13:10 +0300

    responsibility fixes again..

--- src/index.jsx
+++ src/index.jsx
@@ -11,11 +11,13 @@ document.title = settings?.team?.name
 // фикс для корректного отображения
 // учитывается высота адресной строки с помощью dvh в css и фикисируется высота контейнера
 document.addEventListener('DOMContentLoaded', () => {
-  const main = document.querySelector('#main-section')
+  if (window.innerWidth < 570) {
+    const main = document.querySelector('#main-section')
 
-  const fixedHeight = main.clientHeight
+    const fixedHeight = main.clientHeight
 
-  main.style.height = `${fixedHeight}px`
+    main.style.height = `${fixedHeight}px`
+  }
 })
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
